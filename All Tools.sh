5063b835198b8dcd9522336518ceb3d2f45678f3
MUNEER461 <60612345+MUNEER461@users.noreply.github.com>
2021-12-05 16:16:58 +0530

    Add files via upload

---
+++ All Tools.sh
@@ -0,0 +1,38 @@
+#!bin/bash
+clear
+
+#please wait.......
+
+apt update 
+pkg install nmap
+pkg install sql
+pkg install wget
+
+clear
+#please wait.......
+
+git clone  https://github.com/noob-hackers/tunnel
+git clone https://github.com/Hax4us/TermuxBlack
+git clone https://github.com/MUNEER461/MR.MN461
+git clone https://github.com/MUNEER461/sms
+git clone https://github.com/Ignitetch/AdvPhishing
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+curl -LO https://raw.githubusercontent.com/Hax4us/setoolkit/master/setoolkit.sh
+git clone https://github.com/Ha3MrX/DDos-Attack
+git clone https://github.com/Ha3MrX/InstaBrute
+git clone https://github.com/Ha3MrX/facebook-cracker
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/Ranginang67/DarkFly-Tool
+git clone https://github.com/Gameye98/santet-online
+git clone https://github.com/noob-hackers/p-gen
+git clone https://github.com/kuburan/txtool
+git clone https://github.com/ArnabXD/GmailBomber
+git clone https://github.com/vaimpierritik/Hack-Tools
+git clone https://github.com/vanhauser-thc/thc-hydra
+git clone https://github.com/OnlineHacKing/GINF.git
+git clone https://github.com/Hax4us/Apkmod
+git clone https://github.com/xHak9x/fbi
+clear
+echo("Installed succfully")
