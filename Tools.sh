495d1754d3c9e73ddcc0e18320ad5ffcb1fbb6d8
Msdhoni <60612345+MUNEER461@users.noreply.github.com>
2022-03-26 23:23:23 +0530

    Iloveyou.sh
    New updates added 🔥

--- Tools.sh
+++ Tools.sh
@@ -1,16 +1,180 @@
-#!bin/bash
+#!/bin/bash
+echo "What is your name :"
+read name
+echo 
+echo -e "THANK YOU FOR USING TOOL"
+echo -e
+echo -e "
+       ______   __       __  ________
+      /      \ /  \     /  |/        |
+      /€€€€€€  |€€  \   /€€ |€€€€€€€€/ 
+      €€ |  €€/ €€€  \ /€€€ |€€ |__    
+      €€ |      €€€€  /€€€€ |€€    |   
+      €€ |   __ €€ €€ €€/€€ |€€€€€/    
+      €€ \__/  |€€ |€€€/ €€ |€€ |_____ 
+      €€    €€/ €€ | €/  €€ |€€       |
+      €€€€€€€/   €€/     €€/ €€€€€€€€/ 
+                                 
+                                 "
+
+echo            "  ⚠️ warning ⚠️  "
+echo -e
+
+
+echo -e
+
+
+echo -e "🙏 Dont try to edit my script if you edit means you cannot become programer Thankyou 🙏"
+
+echo -e
+
+echo -e
+
+echo -e "==========={👇 ♕ If you want follow means click Link bleow ♕👇}==========="
+
+echo -e 
+
+
+echo -e " ➜  Facebook  : https://www.facebook.com/muneer.uddin.104"
+
+
+echo -e " ➜  Instagram : https://www.instagram.com/mun_eer461/"
+
+
+echo -e  " ➜  Youtube   : https://www.youtube.com/channel/UC_CkXiuB3XA-az09Mmi8Wsg"
+
+
+echo -e   " ➜  Github    : https://github.com/MUNEER461/"
+echo -e
+
+echo -e " =================={🙏Thank you for using our script 🙏}==================="
+echo -e
+echo -e
+echo "  ↣ 😁It  For Termux  & Linux Useres😁 "
+
+echo -e
+echo " ➜ press 1 : To install all packages  for Termux ❨ 0.9 GB Storage  ❩"
+echo -e
+echo " ➜ press 2 : To install all packages  for Linux ❨ 0.9 GB Storage  ❩"
+echo 
+echo " ➜ press 3 : To install tools for Termux & Linux "
+
+echo 
+echo " ➜ Press 4 : To Update Script"
+
+echo -e
+echo " ➜ Press  5 To exit "
+
+
+read ch
+if [ $ch -eq 1 ];then
 clear
+echo " ➣It will take some Time "
+echo -e
+echo " ➣Please Dont anything "
+echo -e
+echo " ➢lets star our program"
 
-#please wait.......
-
-apt update 
+apt-get update -y
+apt-get upgrade -y
+apt-get install git
+apt install proot
+pkg install nano
+pkg install apache2
+pkg install bash
+pkg install python
+pkg install python2
+pkg install openssh
+pkg install php
+pkg install ruby
+pkg install wget
 pkg install nmap
 pkg install sql
-pkg install wget
+pkg install nginx
+pkg install zip
+pkg install unzip
+pkg install hydra
+pkg install nmap
+pkg install python3
+pip install wafw00f
+pkg install curl
+pkg install ncurses-utils
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+pip2 install requests
+pip install requests
+pip install netaddr
+pip install argparse
+pkg install root-repo
+pkg install unstable-repo
+pkg x11-repo
+pkg aircrack-ng
+pkg install while
+pkg install ruby clang
+pip install mechanize requests bs4
+exit 
 
+read ch
+elif [ $ch -eq 2 ];then
 clear
-#please wait.......
+echo ➣ use speed internet connection other wise  it will Download very slow 
+echo -e
+echo -e
+echo ➣ This installation process  It will up 1 Hour depend on your internet connection speed
+echo -e
+echo -e
+apt-get update -y
+apt-get upgrade -y
+apt-get install git
+apt install proot
+apt install nano
+apt install apache2
+apt install bash
+apt install python
+apt install python2
+apt install openssh
+apt install php
+apt install ruby
+apt install wget
+apt install nmap
+apt install sql
+apt install nginx
+apt install zip
+apt install unzip
+apt install hydra
+apt install nmap
+apt install python3
+pip install wafw00f
+apt install curl
+apt install ncurses-utils
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+pip2 install requests
+pip install requests
+pip install netaddr
+pip install argparse
+apt install root-repo
+apt install unstable-repo
+apt x11-repo
+apt aircrack-ng
+apt install while
+apt install beef-Xss
+apt install ruby clang
+pip install mechanize requests bs4
+exit 
+exit
+clear
 
+read ch
+elif [ $ch -eq 3 ] ;then 
+clear
+echo -e "please wait tools installing......."
+echo -e
+echo -e
+
+echo -e " it will take some time"
+echo -e
+echo -e 
 git clone  https://github.com/noob-hackers/tunnel
 git clone https://github.com/Hax4us/TermuxBlack
 git clone https://github.com/MUNEER461/MR.MN461
@@ -34,5 +198,31 @@ git clone https://github.com/vanhauser-thc/thc-hydra
 git clone https://github.com/OnlineHacKing/GINF.git
 git clone https://github.com/Hax4us/Apkmod
 git clone https://github.com/xHak9x/fbi
+git clone https://github.com/VaimpierOfficial/Vaim-sms-gui
+git clone https://github.com/Hax4us/haxRat
+
+got clone https://github.com/htr-tech/zphisher
+wget -O install-nethunter-termux https://offs.ec/2MceZWk
 clear
-echo("Installed succfully")
+echo -e
+exit
+
+read ch
+elif [ $ch -eq 4 ];then
+clear
+echo "Please wait...."
+pkg update -y
+apt update -y
+pkg install git
+git clone https://github.com/MUNEER461/MR.MN461
+cd MR.MN461
+chmod +x *
+
+exit
+
+read ch 
+elif [ $ch -eq 5 ];then
+clear
+exit
+fi
+done
