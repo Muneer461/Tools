c54d1536903ab7a110ce5b386323758b738c91d6
Msdhoni <60612345+MUNEER461@users.noreply.github.com>
2022-03-27 17:23:40 +0530

    Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
-echo "What is your name :"
-read name
-echo 
-echo -e "THANK YOU FOR USING TOOL"
+#echo "What is your name :"
+#read name
+echo -e
+clear
+echo -e "                          THANK YOU FOR USING TOOL                   "
 echo -e
 echo -e "
-       ______   __       __  ________
-      /      \ /  \     /  |/        |
-      /€€€€€€  |€€  \   /€€ |€€€€€€€€/ 
-      €€ |  €€/ €€€  \ /€€€ |€€ |__    
-      €€ |      €€€€  /€€€€ |€€    |   
-      €€ |   __ €€ €€ €€/€€ |€€€€€/    
-      €€ \__/  |€€ |€€€/ €€ |€€ |_____ 
-      €€    €€/ €€ | €/  €€ |€€       |
-      €€€€€€€/   €€/     €€/ €€€€€€€€/ 
-                                 
-                                 "
-
-echo            "  ⚠️ warning ⚠️  "
-echo -e
-
+                  ___   _     _____     _______  __   _____  _   _
+                 |_ _| | |   / _ \ \   / / ____| \ \ / / _ \| | | |
+                  | |  | |  | | | \ \ / /|  _|    \ V / | | | | | |
+                  | |  | |__| |_| |\ V / | |___    | || |_| | |_| |
+                 |___| |_____\___/  \_/  |_____|   |_| \___/ \___/ "
 
 echo -e
-
-
-echo -e 
-echo -e
-
 echo -e
 
 echo -e "==========={👇 ♕ If you want follow means click Link bleow ♕👇}==========="
@@ -52,27 +38,33 @@ echo -e
 echo "  ↣ 😁It  For Termux  & Linux Useres😁 "
 
 echo -e
-echo " ➜ press 1 : To install all packages  for Termux ❨ 0.9 GB Storage  ❩"
 echo -e
-echo " ➜ press 2 : To install all packages  for Linux ❨ 0.9 GB Storage  ❩"
-echo 
-echo " ➜ press 3 : To install tools for Termux & Linux "
+echo -e " ➜ press 1 : To install all packages  for Termux ❨ 0.9 GB to Download ❩"
+#echo -e
+echo -e " ➜ press 2 : To install all packages  for Linux ❨ 0.9 GB to Download  ❩"
+#echo -e
+echo -e " ➜ press 3 : To install tools for Termux & Linux (25 mb to Download "
+#echo -e
+echo -e " ➜ Press 4 : To install Black Termux & Lemon Tool (only for termux users) (600mb to Download )"
+#echo -e
+echo -e  " ➜ Press 5 : To install Metasploit for  Linux ( 800mb to Download )" 
+#echo -e
 
-echo 
-echo " ➜ Press 4 : To Update Script"
+echo -e " ➜ Press 6 : To update Script"
 
+echo -e " ➜ Press 7 : To Exit "
 echo -e
-echo " ➜ Press  5 To exit "
+echo -e
 
+echo -e " Enter Your options : "
+echo -e
 
 read ch
 if [ $ch -eq 1 ];then
 clear
 echo " ➣It will take some Time "
 echo -e
-echo " ➣Please Dont anything "
-echo -e
-echo " ➢lets star our program"
+echo " ➢Lets start"
 
 apt-get update -y
 apt-get upgrade -y
@@ -109,19 +101,17 @@ pkg install unstable-repo
 pkg x11-repo
 pkg aircrack-ng
 pkg install while
-termux-setup-storage
 pkg install ruby clang
 pip install mechanize requests bs4
-chmod +x *
 exit 
 
 read ch
 elif [ $ch -eq 2 ];then
 clear
-echo ➣ use speed internet connection other wise  it will Download very slow 
+echo -e "➣ use speed internet connection other wise  it will Download very slow 
 echo -e
 echo -e
-echo ➣ This installation process  It will up 1 Hour depend on your internet connection speed
+echo -e "➣ This installation process  It will up 1 Hour depend on your internet connection speed
 echo -e
 echo -e
 apt-get update -y
@@ -129,7 +119,6 @@ apt-get upgrade -y
 apt-get install git
 apt install proot
 apt install nano
-termux-setup-storage
 apt install apache2
 apt install bash
 apt install python
@@ -163,20 +152,16 @@ apt install while
 apt install beef-Xss
 apt install ruby clang
 pip install mechanize requests bs4
-chmod +x *
-
 exit 
 exit
 clear
-
 read ch
-elif [ $ch -eq 3 ] ;then 
+elif [ $ch -eq 3 ];then 
 clear
-echo -e "please wait tools installing......."
+echo -e " ➣ please wait tools installing......."
 echo -e
 echo -e
-
-echo -e " it will take some time"
+echo -e " ➣ it will take some time.........."
 echo -e
 echo -e 
 cd
@@ -188,7 +173,6 @@ git clone https://github.com/Ignitetch/AdvPhishing
 git clone https://github.com/thewhiteh4t/seeker
 git clone https://github.com/Rajkumrdusad/Tool-X.git
 curl -LO https://raw.githubusercontent.com/Hax4us/setoolkit/master/setoolkit.sh
-git clone https://github.com/aniketstark/STARK2.0
 git clone https://github.com/Ha3MrX/DDos-Attack
 git clone https://github.com/Ha3MrX/InstaBrute
 git clone https://github.com/Ha3MrX/facebook-cracker
@@ -198,38 +182,76 @@ git clone https://github.com/Ranginang67/DarkFly-Tool
 git clone https://github.com/Gameye98/santet-online
 git clone https://github.com/noob-hackers/p-gen
 git clone https://github.com/kuburan/txtool
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/d093w1z/sayhello
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/htr-tech/track-ip
+git clone https://github.com/KasRoudra/autoreport
+
 git clone https://github.com/ArnabXD/GmailBomber
+git clone https://github.com/vaimpierritik/Hack-Tools
 git clone https://github.com/vanhauser-thc/thc-hydra
 git clone https://github.com/OnlineHacKing/GINF.git
 git clone https://github.com/Hax4us/Apkmod
 git clone https://github.com/xHak9x/fbi
 git clone https://github.com/VaimpierOfficial/Vaim-sms-gui
 git clone https://github.com/Hax4us/haxRat
-git clone https://github.com/king-hacking/SPY-KING
 git clone https://github.com/htr-tech/zphisher
-wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
 wget -O install-nethunter-termux https://offs.ec/2MceZWk
-termux-setup-storage
-chmod +x *
 clear
 echo -e
 exit
-
 read ch
 elif [ $ch -eq 4 ];then
-clear
-echo "Please wait...."
+echo -e 
+echo -e " ➣ please wait...."
+echo -e
 echo -e
 cd
-termux-setup-storage
-git clone https://github.com/MUNEER461/MR.MN461
+apt update
+pkg update 
+apt upgrade
+pkg upgrade
+wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
 chmod +x *
-
-exit
+bash install.sh -i
+clear
+echo -e
+echo -e
+echo -e " ➣ Please wait ........it will take  20mins"
+echo -e
+echo -e
+apt install lemon
+clear
+exit 
 
 read ch 
 elif [ $ch -eq 5 ];then
 clear
+echo -e " ➣ Please wait...."
+echo -e " ➣  It will 50 mins......"
+echo -e
+apt install metasploit 
+clear
+exit
+
+elif [ $ch -eq 6 ];then
+echo -e
+clear
+echo -e
+echo -e
+echo " ➣ Please wait...."
+cd
+pkg update -y
+apt update -y
+pkg install git
+git clone https://github.com/MUNEER461/AllTools
+clear
+exit
+
+read ch 
+elif [ $ch -eq 7 ];then
+clear
 exit
 fi
-done
+Done 
