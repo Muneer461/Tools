d9bdf2ecc17b3594f23a124ec48ceadb527098bc
Msdhoni <60612345+MUNEER461@users.noreply.github.com>
2022-03-31 10:03:36 +0530

    Tools.sh

--- Tools.sh
+++ Tools.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#Script developed by Mun_eer461
-#echo "What is your name :"
+#script developed by mun_eer461
+#echo "what is your name :"
 #read name
 echo 
 clear
-echo  "                          THANK YOU FOR USING TOOL                   "
+echo  "                          thank you for using tool                   "
 
 echo 
 echo  "
@@ -26,66 +26,66 @@ echo  "
 echo  "
                   ___   _     _____     _______  __   _____  _   _
                  |_ _| | |   / _ \ \   / / ____| \ \ / / _ \| | | |
-                  | |  | |  | | | \ \ / /|  _|    \ V / | | | | | |
-                  | |  | |__| |_| |\ V / | |___    | || |_| | |_| |
+                  | |  | |  | | | \ \ / /|  _|    \ v / | | | | | |
+                  | |  | |__| |_| |\ v / | |___    | || |_| | |_| |
                  |___| |_____\___/  \_/  |_____|   |_| \___/ \___/ "
 
 echo 
 echo 
 
-echo  "==========={👇 ♕ If you want follow means click Link bleow ♕👇}==========="
+echo  "==========={👇 ♕ if you want follow means click link bleow ♕👇}==========="
 
 echo  
 
 
-echo  " ➜  Facebook  : https://www.facebook.com/muneer.uddin.104"
+echo  " ➜  facebook  : https://www.facebook.com/muneer.uddin.104"
 
 
-echo  " ➜  Instagram : https://www.instagram.com/mun_eer461/"
+echo  " ➜  instagram : https://www.instagram.com/mun_eer461/"
 
 
-echo  " ➜  Youtube   : https://www.youtube.com/channel/UC_CkXiuB3XA-az09Mmi8Wsg"
+echo  " ➜  youtube   : https://www.youtube.com/channel/uc_ckxiub3xa-az09mmi8wsg"
 
 
-echo  " ➜  Github    : https://github.com/MUNEER461/"
-echo  " ➜  twitter   : https://twitter.com/Muneer461/"
+echo  " ➜  github    : https://github.com/muneer461/"
+echo  " ➜  twitter   : https://twitter.com/muneer461/"
 echo 
 echo 
 
-echo   " =================={🙏Thank you for using our script 🙏}==================="
+echo   " =================={🙏thank you for using our script 🙏}==================="
 echo 
 echo 
-echo "  ↣ 😁It  For Termux  & Linux Useres😁 "
+echo "  ↣ 😁it  for termux  & linux useres😁 "
 
 echo 
 echo 
-echo   " ➜ press 1 : To install all packages  for Termux ❨ 0.9 GB to Download ❩"
+echo   " ➜ press 1 : to install all packages  for termux ❨ 0.9 gb to download ❩"
 #echo 
-echo   " ➜ press 2 : To install all packages  for Linux ❨ 0.9 GB to Download  ❩"
-
-echo   " ➜ press 3 : To install tools for Termux & Linux (25 mb to Download "
+echo   " ➜ press 2 : to install all packages  for linux ❨ 0.9 gb to download  ❩"
+echo    " ➜ press 3 : to install all packages  for linux sudo users ❨ 0.9 gb to download  ❩"
+echo   " ➜ press 4 : to install tools for termux & linux (25 mb to download "
 #echo -e
-echo   " ➜ Press 4 : To install Black Termux & Lemon Tool (For Termux users) (600mb Download )"
+echo   " ➜ press 5 : to install black termux & lemon tool (for termux users) (600mb download )"
 #echo -e
-echo   " ➜ Press 5 : To install Metasploit for  Linux ( 800mb to Download )"
-echo   " ➜ Prees 6 : To install Metasploit for  Termux ( 800mb to Download )"
+echo   " ➜ press 6 : to install metasploit for  linux ( 800mb to download )"
+echo   " ➜ prees 7 : to install metasploit for  termux ( 800mb to download )"
 #echo -e
 
-echo   " ➜ Press 7 : To update Script"
+echo   " ➜ press 8 : to update script"
 
-echo   " ➜ Press 8 : To Exit "
+echo   " ➜ press 9 : to exit "
 echo 
 echo 
 
-echo  " Enter Your options : "
+echo  " enter your options : "
 echo 
 
 read ch
 if [ $ch -eq 1 ];then
 clear
-echo " ➣It will take some Time "
+echo " ➣it will take some time "
 echo 
-echo " ➢Lets start"
+echo " ➢lets start"
 echo 
 echo 
 apt-get update -y
@@ -130,10 +130,10 @@ exit
 read ch
 elif [ $ch -eq 2 ];then
 clear
-echo    "➣ use speed internet connection other wise  it will Download very slow "
+echo    "➣ use speed internet connection other wise  it will download very slow "
 echo 
 echo 
-echo    "➣ This installation process  It will up 1 Hour depend on your internet connection speed"
+echo    "➣ this installation process  it will up 1 hour depend on your internet connection speed"
 echo 
 echo 
 apt-get update -y
@@ -171,14 +171,66 @@ apt install unstable-repo
 apt x11-repo
 apt aircrack-ng
 apt install while
-apt install beef-Xss
+apt install beef-xss
 apt install ruby clang
 pip install mechanize requests bs4
 exit 
 exit
 clear
 read ch
+
 elif [ $ch -eq 3 ];then 
+clear
+echo   " ➣ please wait packages installing......."
+echo 
+echo 
+echo    " ➣ it will take some time.........."
+echo 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git
+sudo apt install proot
+sudo apt install nano
+sudo apt install apache2
+sudo apt install bash
+sudo apt install python
+sudo apt install python2
+sudo apt install openssh
+sudo apt install php
+sudo apt install ruby
+sudo apt install wget
+sudo apt install nmap
+sudo apt install sql
+sudo apt install nginx
+sudo apt install zip
+sudo apt install unzip
+sudo apt install hydra
+sudo apt install nmap
+sudo apt install python3
+sudo pip install wafw00f
+sudo apt install curl
+sudo apt install ncurses-utils
+sudo pip install -r requirements.txt
+sudo pip2 install -r requirements.txt
+sudo pip2 install requests
+sudo pip install requests
+sudo pip install netaddr
+sudo pip install argparse
+sudo apt install root-repo
+sudo apt install unstable-repo
+sudo apt x11-repo
+sudo apt aircrack-ng
+sudo apt install while
+sudo apt install beef-xss
+sudo apt install ruby clang
+sudo pip install mechanize requests bs4
+clear 
+exit
+
+
+read ch
+
+elif [ $ch -eq 4];then 
 clear
 echo   " ➣ please wait tools installing......."
 echo 
@@ -188,42 +240,42 @@ echo
 echo    
 cd
 git clone  https://github.com/noob-hackers/tunnel
-git clone https://github.com/Hax4us/TermuxBlack
-git clone https://github.com/MUNEER461/MR.MN461
-git clone https://github.com/MUNEER461/sms
-git clone https://github.com/Ignitetch/AdvPhishing
+git clone https://github.com/hax4us/termuxblack
+git clone https://github.com/muneer461/mr.mn461
+git clone https://github.com/muneer461/sms
+git clone https://github.com/ignitetch/advphishing
 git clone https://github.com/thewhiteh4t/seeker
-git clone https://github.com/Rajkumrdusad/Tool-X.git
-curl -LO https://raw.githubusercontent.com/Hax4us/setoolkit/master/setoolkit.sh
-git clone https://github.com/Ha3MrX/DDos-Attack
-git clone https://github.com/Ha3MrX/InstaBrute
-git clone https://github.com/Ha3MrX/facebook-cracker
+git clone https://github.com/rajkumrdusad/tool-x.git
+curl -lo https://raw.githubusercontent.com/hax4us/setoolkit/master/setoolkit.sh
+git clone https://github.com/ha3mrx/ddos-attack
+git clone https://github.com/ha3mrx/instabrute
+git clone https://github.com/ha3mrx/facebook-cracker
 git clone https://github.com/hangetzzu/saycheese
 git clone https://github.com/noob-hackers/hacklock
-git clone https://github.com/Ranginang67/DarkFly-Tool
-git clone https://github.com/Gameye98/santet-online
+git clone https://github.com/ranginang67/darkfly-tool
+git clone https://github.com/gameye98/santet-online
 git clone https://github.com/noob-hackers/p-gen
 git clone https://github.com/kuburan/txtool
 git clone https://github.com/hangetzzu/saycheese
 git clone https://github.com/d093w1z/sayhello
 git clone https://github.com/sherlock-project/sherlock
 git clone https://github.com/htr-tech/track-ip
-git clone https://github.com/KasRoudra/autoreport
-git clone https://github.com/ArnabXD/GmailBomber
-git clone https://github.com/vaimpierritik/Hack-Tools
+git clone https://github.com/kasroudra/autoreport
+git clone https://github.com/arnabxd/gmailbomber
+git clone https://github.com/vaimpierritik/hack-tools
 git clone https://github.com/vanhauser-thc/thc-hydra
-git clone https://github.com/OnlineHacKing/GINF.git
-git clone https://github.com/Hax4us/Apkmod
-git clone https://github.com/xHak9x/fbi
-git clone https://github.com/VaimpierOfficial/Vaim-sms-gui
-git clone https://github.com/Hax4us/haxRat
+git clone https://github.com/onlinehacking/ginf.git
+git clone https://github.com/hax4us/apkmod
+git clone https://github.com/xhak9x/fbi
+git clone https://github.com/vaimpierofficial/vaim-sms-gui
+git clone https://github.com/hax4us/haxrat
 git clone https://github.com/htr-tech/zphisher
-wget -O install-nethunter-termux https://offs.ec/2MceZWk
+wget -o install-nethunter-termux https://offs.ec/2mcezwk
 clear
 echo 
 exit
 read ch
-elif [ $ch -eq 4 ];then
+elif [ $ch -eq 5 ];then
 clear
 echo  
 echo    " ➣ please wait...."
@@ -234,13 +286,13 @@ apt update
 pkg update 
 apt upgrade
 pkg upgrade
-wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
+wget https://github.com/hax4us/termuxblack/raw/master/install.sh
 chmod +x *
 bash install.sh -i
 clear
 echo 
 echo 
-echo    " ➣ Please wait ........it will take  20mins"
+echo    " ➣ please wait ........it will take  20mins"
 echo             
 echo 
 apt install lemon
@@ -248,16 +300,16 @@ clear
 exit 
 
 read ch 
-elif [ $ch -eq 5 ];then
+elif [ $ch -eq 6 ];then
 clear
-echo    " ➣ Please wait...."
-echo    " ➣  It will 50 mins......"
+echo    " ➣ please wait...."
+echo    " ➣  it will 50 mins......"
 echo 
 apt install metasploit 
 clear
 exit
 
-elif [ $ch -eq 6 ];then
+elif [ $ch -eq 7 ];then
 echo 
 clear
 cd 
@@ -267,33 +319,33 @@ pkg install wget
 
 clear
 echo 
-echo    " ➣ Please wait...."
+echo    " ➣ please wait...."
 echo 
-wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+wget https://raw.githubusercontent.com/hax4us/metasploit_termux/master/metasploit.sh
 chmod +x metasploit
 bash metasploit.sh
 echo 
 clear
 
 
-elif [ $ch -eq 7 ];then
+elif [ $ch -eq 8 ];then
 echo 
 clear
 echo 
 echo 
-echo " ➣ Please wait...."
+echo " ➣ please wait...."
 cd
 pkg update -y
 apt update -y
 pkg install git
-git clone https://github.com/MUNEER461/AllTools
+git clone https://github.com/muneer461/alltools
 clear
 exit
 
 read ch 
-elif [ $ch -eq 8 ];then
+elif [ $ch -eq 9 ];then
 clear
 exit
 fi
-Done 
+done 
  
