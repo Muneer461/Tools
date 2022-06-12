6feeec43f564c431ef3b63c327a734bbc0fc07c7
lucifier467 <60612345+MUNEER461@users.noreply.github.com>
2022-06-12 18:29:30 +0530

    Create Tools V2.sh

---
+++ Tools V2.sh
@@ -0,0 +1,408 @@
+#!/bin/bash
+#script developed by mun_eer461
+#echo "what is your name :"
+#read name
+echo 
+clear
+echo  "                          thank you for using tool                   "
+
+echo 
+echo  "
+                         ────╪███████╪────╪███████
+                         ──╪███████████╪╪███████████
+                         ──██████████████████████████
+                         ─████████████████████████████
+                         ─████████████████████████████
+                         ─████████████████████████████
+                         ─████████████████████████████
+                         ──██████████████████████████
+                         ──╪████████████████████████
+                         ───╪██████████████████████
+                         ─────████████████████████
+                          ──────╪████████████████
+                           ────────╪████████████
+                           ──────────╪████████
+                           ─────────────╪██"
+echo  "
+                  ___   _     _____     _______  __   _____  _   _
+                 |_ _| | |   / _ \ \   / / ____| \ \ / / _ \| | | |
+                  | |  | |  | | | \ \ / /|  _|    \ v / | | | | | |
+                  | |  | |__| |_| |\ v / | |___    | || |_| | |_| |
+                 |___| |_____\___/  \_/  |_____|   |_| \___/ \___/ "
+
+echo 
+echo 
+
+echo  "==========={👇 ♕ if you want follow means click link bleow ♕👇}==========="
+
+echo  
+
+
+echo  " ➜  facebook  : https://www.facebook.com/muneer.uddin.104"
+
+
+echo  " ➜  instagram : https://www.instagram.com/mun_eer461/"
+
+
+echo  " ➜  youtube   : https://www.youtube.com/channel/uc_ckxiub3xa-az09mmi8wsg"
+
+
+echo  " ➜  github    : https://github.com/muneer461/"
+echo  " ➜  twitter   : https://twitter.com/muneer461/"
+echo 
+echo 
+
+echo   " =================={🙏thank you for using our script 🙏}==================="
+echo 
+echo 
+echo "  ↣ 😁it  for termux  & linux useres😁 "
+
+echo 
+echo 
+echo   " ➜ press 1 : to install all packages  for termux ❨ 0.9 gb to download ❩"
+#echo 
+echo   " ➜ press 2 : to install all packages  for linux ❨ 0.9 gb to download  ❩"
+echo    " ➜ press 3 : to install all packages  for linux sudo users ❨ 0.9 gb to download  ❩"
+echo   " ➜ press 4 : to install tools for Termux (25 mb to download)"
+echo   " ➜ press 5 : to install tools for  Linux (25 mb to download) "
+#echo -e
+echo   " ➜ press 6 : to install black termux & lemon tool (for termux users) (600mb download )"
+#echo -e
+echo   " ➜ press 7 : to install metasploit for  linux ( 800mb to download )"
+echo   " ➜ prees 8 : to install metasploit for  termux ( 800mb to download )"
+echo 
+echo   " ➜ press 9 : to update script"
+
+echo   " ➜ press 10 : to exit "
+echo 
+echo 
+
+echo  " enter your options : "
+echo 
+
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo " ➣it will take some time "
+echo 
+echo " ➢lets start"
+echo 
+echo 
+apt-get update -y
+apt-get upgrade -y
+apt-get install git
+apt install proot
+pkg install nano
+pkg install apache2
+pkg install bash
+pkg install python
+pkg install python2
+pkg install openssh
+pkg install php
+pkg install ruby
+pkg install wget
+pkg install nmap
+pkg install sql
+pkg install nginx
+pkg install zip
+pkg install unzip
+pkg install hydra
+pkg install nmap
+pkg install python3
+pip install wafw00f
+pkg install curl
+pkg install ncurses-utils
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+pip2 install requests
+pip install requests
+pip install netaddr
+pip install argparse
+pkg install root-repo
+pkg install unstable-repo
+pkg x11-repo
+pkg aircrack-ng
+pkg install while
+pkg install ruby clang
+pip install mechanize requests bs4
+clear
+exit 
+
+read ch
+elif [ $ch -eq 2 ];then
+clear
+echo    "➣ use speed internet connection other wise  it will download very slow "
+echo 
+echo 
+echo    "➣ this installation process  it will up 1 hour depend on your internet connection speed"
+echo 
+echo 
+apt-get update -y
+apt-get upgrade -y
+apt-get install git
+apt install proot
+apt install nano
+apt install apache2
+apt install bash
+apt install python
+apt install python2
+apt install openssh
+apt install php
+apt install ruby
+apt install wget
+apt install nmap
+apt install sql
+apt install nginx
+apt install zip
+apt install unzip
+apt install hydra
+apt install nmap
+apt install python3
+pip install wafw00f
+apt install curl
+apt install ncurses-utils
+pip install -r requirements.txt
+pip2 install -r requirements.txt
+pip2 install requests
+pip install requests
+pip install netaddr
+pip install argparse
+apt install root-repo
+apt install unstable-repo
+apt x11-repo
+apt aircrack-ng
+apt install while
+apt install beef-xss
+apt install ruby clang
+pip install mechanize requests bs4
+exit 
+exit
+clear
+read ch
+
+elif [ $ch -eq 3 ];then 
+clear
+echo   " ➣ please wait packages installing......."
+echo 
+echo 
+echo    " ➣ it will take some time.........."
+echo 
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install git
+sudo apt install proot
+sudo apt install nano
+sudo apt install apache2
+sudo apt install bash
+sudo apt install python
+sudo apt install python2
+sudo apt install openssh
+sudo apt install php
+sudo apt install ruby
+sudo apt install wget
+sudo apt install nmap
+sudo apt install sql
+sudo apt install nginx
+sudo apt install zip
+sudo apt install unzip
+sudo apt install hydra
+sudo apt install nmap
+sudo apt install python3
+sudo pip install wafw00f
+sudo apt install curl
+sudo apt install ncurses-utils
+sudo pip install -r requirements.txt
+sudo pip2 install -r requirements.txt
+sudo pip2 install requests
+sudo pip install requests
+sudo pip install netaddr
+sudo pip install argparse
+sudo apt install root-repo
+sudo apt install unstable-repo
+sudo apt x11-repo
+sudo apt aircrack-ng
+sudo apt install while
+sudo apt install beef-xss
+sudo apt install ruby clang
+sudo pip install mechanize requests bs4
+clear 
+exit
+
+
+read ch
+
+elif [ $ch -eq 4 ];then 
+clear
+echo   " ➣ please wait tools installing......."
+echo 
+echo 
+echo    " ➣ it will take some time.........."
+echo 
+echo    
+cd
+git clone  https://github.com/noob-hackers/tunnel
+git clone https://github.com/hax4us/termuxblack
+git clone https://github.com/muneer461/mr.mn461
+git clone https://github.com/muneer461/sms
+git clone https://github.com/ignitetch/advphishing
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/rajkumrdusad/tool-x.git
+curl -lo https://raw.githubusercontent.com/hax4us/setoolkit/master/setoolkit.sh
+git clone https://github.com/ha3mrx/ddos-attack
+git clone https://github.com/ha3mrx/instabrute
+git clone https://github.com/ha3mrx/facebook-cracker
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/ranginang67/darkfly-tool
+git clone https://github.com/gameye98/santet-online
+git clone https://github.com/noob-hackers/p-gen
+git clone https://github.com/kuburan/txtool
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/d093w1z/sayhello
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/htr-tech/track-ip
+git clone https://github.com/kasroudra/autoreport
+git clone https://github.com/arnabxd/gmailbomber
+git clone https://github.com/vanhauser-thc/thc-hydra
+git clone https://github.com/onlinehacking/ginf.git
+git clone https://github.com/hax4us/apkmod
+git clone https://github.com/xhak9x/fbi
+git clone https://github.com/VaimpierOfficial/Vaim-Samay
+git clone https://github.com/vaimpierofficial/vaim-sms-gui
+git clone https://github.com/hax4us/haxrat
+git clone https://github.com/VaimpierOfficial/Vaim-RamSom
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/samay825/Sms-Bomb
+git clone https://github.com/d093w1z/sayhello
+git clone https://github.com/Elliot290/BaapGAttack
+git clone https://github.com/trustedsec/social-engineer-toolkit
+git clone https://github.com/samay825/Email-Thread
+git clone https://github.com/Ign0r3dH4x0r/Xweapon
+wget -o install-nethunter-termux https://offs.ec/2mcezwk
+clear
+echo 
+exit
+read ch
+elif [ $ch  -eq 5 ];then
+cd
+git clone  https://github.com/noob-hackers/tunnel
+git clone https://github.com/hax4us/termuxblack
+git clone https://github.com/muneer461/mr.mn461
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/Elliot290/BaapGAttack
+git clone https://github.com/muneer461/sms
+git clone https://github.com/d093w1z/sayhello
+git clone https://github.com/Ign0r3dH4x0r/Xweapon
+git clone https://github.com/ignitetch/advphishing
+git clone https://github.com/thewhiteh4t/seeker
+git clone https://github.com/VaimpierOfficial/Vaim-Samay
+git clone https://github.com/VaimpierOfficial/Vaim-RamSom
+git clone https://github.com/rajkumrdusad/tool-x.git
+git clone https://github.com/ha3mrx/ddos-attack
+git clone https://github.com/ha3mrx/instabrute
+git clone https://github.com/ha3mrx/facebook-cracker
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/noob-hackers/hacklock
+git clone https://github.com/ranginang67/darkfly-tool
+git clone https://github.com/gameye98/santet-online
+git clone https://github.com/noob-hackers/p-gen
+git clone https://github.com/samay825/Email-Thread
+git clone https://github.com/kuburan/txtool
+git clone https://github.com/hangetzzu/saycheese
+git clone https://github.com/d093w1z/sayhello
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/htr-tech/track-ip
+git clone https://github.com/kasroudra/autoreport
+git clone https://github.com/arnabxd/gmailbomber
+git clone https://github.com/vanhauser-thc/thc-hydra
+git clone https://github.com/onlinehacking/ginf.git
+git clone https://github.com/hax4us/apkmod
+git clone https://github.com/xhak9x/fbi
+git clone https://github.com/vaimpierofficial/vaim-sms-gui
+git clone https://github.com/hax4us/haxrat
+git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/samay825/Sms-Bomb
+clear
+exit 
+
+read ch
+elif [ $ch -eq 6 ];then
+clear
+echo  
+echo    " ➣ please wait...."
+echo      
+echo 
+cd
+apt update
+pkg update 
+apt upgrade
+pkg upgrade
+wget https://github.com/hax4us/termuxblack/raw/master/install.sh
+chmod +x *
+bash install.sh -i
+clear
+echo 
+echo 
+echo    " ➣ please wait ........it will take  20mins"
+echo             
+echo 
+apt install lemon
+clear
+exit 
+
+read ch 
+elif [ $ch -eq 7 ];then
+clear
+echo    " ➣ please wait...."
+echo    " ➣  it will 50 mins......"
+echo 
+cd 
+apt install metasploit 
+clear
+exit
+
+elif [ $ch -eq 8 ];then
+echo 
+clear
+cd 
+pkg install git
+pkg install bash
+pkg install wget 
+
+clear
+echo 
+echo    " ➣ please wait...."
+echo 
+wget https://raw.githubusercontent.com/hax4us/metasploit_termux/master/metasploit.sh
+chmod +x metasploit
+bash metasploit.sh
+echo 
+clear
+
+
+elif [ $ch -eq 9 ];then
+echo 
+clear
+echo 
+echo 
+echo " ➣ please wait...."
+cd
+pkg update -y
+apt update -y
+pkg install git
+rm -fr Tools
+git clone https://github.com/muneer461/alltools
+cd tools
+chmod +x *
+bash Tools.sh
+clear
+exit
+
+read ch 
+elif [ $ch -eq 10 1];then
+clear
+exit
+fi
+done 
+ 
